2c1bc4c2464efc6797a48326e8159c61484cc209
neander <neander2801@gmail.com>
2023-03-02 14:14:38 -0300

    chore: added routes in app.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage"
-import './App.css'
+import LandingPage from "./pages/LandingPage";
+import "./App.css";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-    </Routes>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
   );
 }
 
