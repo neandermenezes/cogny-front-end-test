5bb47058e85f4fb59826b7894bdebb283cea6181
neander <neander2801@gmail.com>
2023-03-02 14:16:01 -0300

    chore: created checkout component

--- web/src/pages/LandingPage.js
+++ web/src/pages/LandingPage.js
@@ -1,46 +1,50 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { db } from '../services/firestore';
-import { collection, getDocs} from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+
+import Header from "../components/Header";
+import ProductsContext from "../context/ProductsContext";
+
+import { db } from "../services/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
 function LandingPage() {
   const [data, setData] = useState([]);
+  const { cart, addCartItem } = useContext(ProductsContext);
 
   async function getProducts() {
     const querySnapshot = await getDocs(collection(db, "produtos"));
 
     const products = querySnapshot.docs.map((doc) => doc.data());
 
-    setData(products)
+    setData(products);
   }
   useEffect(() => {
     getProducts();
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Shoe Store</h1>
-      </header>
-      <main>
-        <section>
-          <h2>Featured Shoes</h2>
-          <div className="shoes-container">
-            {data.map((shoe, index) => (
-              <div className="shoe-card" key={index}>
-                <img src={shoe.image_url} alt={shoe.description} />
-                <h3>{shoe.description}</h3>
-                <p>{shoe.price}</p>
-                <button>Add to Cart</button>
-              </div>
-            ))}
+    <>
+      <Header />
+      <main className="main">
+        {data.map((shoe, index) => (
+          <div className="shoe-card" key={index}>
+            <img
+              className="img-shoe"
+              src={shoe.image_url}
+              alt={shoe.description}
+            />
+            <h3 className="shoe-info">{shoe.description}</h3>
+            <p className="price">{shoe.price}</p>
+            <button
+              className="btn-add"
+              onClick={() => addCartItem({ ...shoe, id: index + 1 })}
+            >
+              <span>1</span>
+              Adicionar ao Carrinho
+            </button>
           </div>
-        </section>
+        ))}
       </main>
-      <footer>
-        <p>&copy; 2023 Shoe Store. All rights reserved.</p>
-      </footer>
-    </div>
+    </>
   );
 }
 
